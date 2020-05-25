240253ea1ae638e96e55d03b97b44137bf3f4827
Ezekiel Ekunola <ekunolaeasybuoy@gmail.com>
2020-05-25 20:12:17 +0100

    add productoverview markup

---
+++ screens/shop/ProductOverview.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, FlatList, StyleSheet} from 'react-native'
+
+const ProductOverview = () => {
+    return (
+        <View>
+            
+        </View>
+    )
+}
+
+export default ProductOverview
