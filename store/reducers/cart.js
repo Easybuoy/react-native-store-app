37cdefcd757b86596cfbfab80925851d375f21af
Ezekiel Ekunola <ekunolaeasybuoy@gmail.com>
2020-05-25 22:18:49 +0100

    setup cart reducer

--- store/reducers/cart.js
+++ store/reducers/cart.js
@@ -1,4 +1,5 @@
 import { ADD_TO_CART } from "../types";
+import CartItem from "../../models/cart-item";
 
 const INITIAL_STATE = {
   items: {},
@@ -10,10 +11,32 @@ export default (state = INITIAL_STATE, action) => {
     case ADD_TO_CART:
       const addedProduct = action.product;
       const productPrice = addedProduct.price;
-      return {
-        ...state,
-        totalAmount: totalAmount++,
-      };
+      const productTitle = addedProduct.title;
+
+      if (state.items[addedProduct.id]) {
+        const updatedCartItem = new CartItem(
+          state.items[addedProduct.id].quantity + 1,
+          productPrice,
+          productTitle,
+          state.items[addedProduct.id].sum + productPrice
+        );
+        return {
+          ...state,
+          items: { ...state.items, [addedProduct.id]: updatedCartItem },
+        };
+      } else {
+        const newCartItem = new CartItem(
+          1,
+          productPrice,
+          productTitle,
+          productPrice
+        );
+        return {
+          ...state,
+          [addedProduct.id]: newCartItem,
+        };
+      }
+
     default:
       return state;
   }
