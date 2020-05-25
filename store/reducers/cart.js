bcbe70a07e689b2343ca755b09f6f87d2b920b11
Ezekiel Ekunola <ekunolaeasybuoy@gmail.com>
2020-05-25 22:10:53 +0100

    setup cart reducer

--- store/reducers/cart.js
+++ store/reducers/cart.js
@@ -1,8 +1,20 @@
+import { ADD_TO_CART } from "../types";
+
 const INITIAL_STATE = {
-    items: {},
-    totalAmount: 0
+  items: {},
+  totalAmount: 0,
 };
 
 export default (state = INITIAL_STATE, action) => {
-  return state;
+  switch (action.type) {
+    case ADD_TO_CART:
+      const addedProduct = action.product;
+      const productPrice = addedProduct.price;
+      return {
+        ...state,
+        totalAmount: totalAmount++,
+      };
+    default:
+      return state;
+  }
 };
