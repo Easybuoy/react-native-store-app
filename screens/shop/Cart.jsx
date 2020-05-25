9a4225970650dac1da90e20c9598b72bfeb4e180
Ezekiel Ekunola <ekunolaeasybuoy@gmail.com>
2020-05-25 22:44:25 +0100

    add cart markup

---
+++ screens/shop/Cart.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { View, StyleSheet, FlatList, Button } from "react-native";
+
+function Cart() {
+  return <div></div>;
+}
+
+const styles = StyleSheet.create({});
+export default Cart;
