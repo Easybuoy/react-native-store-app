8102e8567bdeb9d87b8069f29e520511719f67c2
Ezekiel Ekunola <ekunolaeasybuoy@gmail.com>
2020-05-25 21:00:57 +0100

    add productitem markup

--- components/ProductItem.jsx
+++ components/ProductItem.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { View, Text, Image, StyleSheet, Button } from "react-native";
 
-const ProductItem = () => {
+import Colors from "../constants/Colors";
+
+const ProductItem = ({ image, title }) => {
   return (
     <View>
-      <Image />
-      <Text>Title</Text>
-      <Text>Price</Text>
+      <Image source={{ uri: image }} />
+      <Text>{title}</Text>
+      <Text>${price.toFixed(2)}</Text>
       <View>
         <Button title="View details" />
         <Button title="add to cart" />
@@ -15,6 +17,18 @@ const ProductItem = () => {
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  product: {
+    shadowColor: Colors.BLACK,
+    shadowOpacity: 0.26,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 8,
+    elevation: 5,
+    borderRadius: 10,
+    backgroundColor: Colors.WHITE,
+    height: 300,
+    margin: 20,
+  },
+});
 
 export default ProductItem;
