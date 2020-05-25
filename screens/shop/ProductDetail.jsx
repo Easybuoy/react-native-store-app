acd0f1c36fb27c2cf0213257aee7d8181a5f8cca
Ezekiel Ekunola <ekunolaeasybuoy@gmail.com>
2020-05-25 21:18:44 +0100

    add productdetail markup

---
+++ screens/shop/ProductDetail.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text, Image, StyleSheet, ScrollView } from "react-native";
+
+const ProductDetail = () => {
+  return <ScrollView></ScrollView>;
+};
+
+const styles = StyleSheet.create({});
+
+export default ProductDetail;
