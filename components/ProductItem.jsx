4b86b5621b75b57129753fd620c9df0525f7121e
Ezekiel Ekunola <ekunolaeasybuoy@gmail.com>
2020-05-25 21:14:36 +0100

    add productitem markup

--- components/ProductItem.jsx
+++ components/ProductItem.jsx
@@ -3,15 +3,29 @@ import { View, Text, Image, StyleSheet, Button } from "react-native";
 
 import Colors from "../constants/Colors";
 
-const ProductItem = ({ image, title }) => {
+const ProductItem = ({ image, title, price, viewDetail, addToCart }) => {
   return (
-    <View>
-      <Image source={{ uri: image }} />
-      <Text>{title}</Text>
-      <Text>${price.toFixed(2)}</Text>
-      <View>
-        <Button title="View details" />
-        <Button title="add to cart" />
+    <View style={styles.product}>
+      <View style={styles.imageContainer}>
+        <Image style={styles.image} source={{ uri: image }} />
+      </View>
+
+      <View style={styles.productDetail}>
+        <Text style={styles.title}>{title}</Text>
+        <Text style={styles.price}>${price.toFixed(2)}</Text>
+      </View>
+
+      <View style={styles.actions}>
+        <Button
+          color={Colors.PRIMARY}
+          title="View details"
+          onPress={viewDetail}
+        />
+        <Button
+          color={Colors.PRIMARY}
+          title="Add to cart"
+          onPress={addToCart}
+        />
       </View>
     </View>
   );
@@ -28,6 +42,34 @@ const styles = StyleSheet.create({
     backgroundColor: Colors.WHITE,
     height: 300,
     margin: 20,
+  },
+  imageContainer: {
+    width: "100%",
+    height: "60%",
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+  title: {
+    fontSize: 18,
+    marginVertical: 3,
+  },
+  price: {
+    fontSize: 14,
+    color: "#888",
+  },
+  actions: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    height: "25%",
+    paddingHorizontal: 10,
+  },
+  productDetail: {
+    alignItems: "center",
+    height: "15%",
+    padding: 10,
   },
 });
 
--- screens/shop/ProductOverview.jsx
+++ screens/shop/ProductOverview.jsx
@@ -2,12 +2,22 @@ import React from "react";
 import { View, FlatList, Text, StyleSheet } from "react-native";
 import { useSelector } from "react-redux";
 
+import ProductItem from "../../components/ProductItem";
+
 const ProductOverview = () => {
   const products = useSelector((state) => state.products.availableProducts);
   return (
     <FlatList
       data={products}
-      renderItem={(itemData) => <Text>{itemData.item.title} </Text>}
+      renderItem={(itemData) => (
+        <ProductItem
+          title={itemData.item.title}
+          image={itemData.item.imageUrl}
+          price={itemData.item.price}
+          viewDetail={() => {}}
+          addToCart={() => {}}
+        />
+      )}
     />
   );
 };
