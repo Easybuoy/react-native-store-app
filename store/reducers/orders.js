28faf8478e2d2960d81d62ba2239825d42fc1a64
Ezekiel Ekunola <ekunolaeasybuoy@gmail.com>
2020-05-26 00:13:11 +0100

    add orders functionality

--- store/reducers/orders.js
+++ store/reducers/orders.js
@@ -1,7 +1,14 @@
+import { ADD_ORDER } from "../types";
+import Order from '../../models/order';
+
 const INITIAL_STATE = {
   orders: [],
 };
 
 export default (state = INITIAL_STATE, action) => {
+    switch(action.types) {
+        case ADD_ORDER:
+            const newOrder = new Order(new Date().toString())
+    }
   return state;
 };
