fc3cb0fb12c2c5122c7f9b92a06f7d99f1966f41
Ezekiel Ekunola <ekunolaeasybuoy@gmail.com>
2020-06-19 12:39:09 +0100

    add auth action

--- store/reducers/index.js
+++ store/reducers/index.js
@@ -3,9 +3,11 @@ import { combineReducers } from "redux";
 import productsReducer from "./products";
 import cartReducer from "./cart";
 import orderReducer from "./orders";
+import authReducer from "./auth";
 
 export default combineReducers({
   products: productsReducer,
   cart: cartReducer,
   orders: orderReducer,
+  auth: authReducer,
 });
